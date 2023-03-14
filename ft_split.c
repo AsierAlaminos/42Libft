cc413a142a13c6a3fee471a1ceb58a3c0746f2dc
Asmus37 <alaminosasier@gmail.com>
2023-03-14 11:30:03 +0100

    update split tests

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: asmus37 <asmus37@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 18:15:28 by asmus37           #+#    #+#             */
-/*   Updated: 2023/03/12 19:47:26 by asmus37          ###   ########.fr       */
+/*   Updated: 2023/03/14 11:29:33 by asmus37          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,92 @@
 #include <string.h>
 #include <stdlib.h>
 
-int	str_count(char const *s, char c)
+static int	str_count(char const *s, char c, int i)
 {
-	int	i;
-
-	i = 0;
+	int	j;
+	
+	j = 0;
 	while (s[i] != c && s[i] != '\0')
-		++i;
-	return (i);
-}
-
-void	add_str(char const *s, char **arr, int len, int pos)
-{
-	int	i;
-
-	i = 0;
-	while (i < len)
 	{
-		arr[pos][i] = s[i];
 		++i;
+		++j;
 	}
+	return (j);
 }
 
-int	str_len(char const *s, char c)
+static int	rem_del(char const *s1, char c)
+{
+	int	comp;
+	int	cles;
+
+	comp = 0;
+	cles = 0;
+	if (*s1 == '\0')
+		return (0);
+	while (*s1 != '\0')
+	{
+		if (*s1 == c)
+			cles = 0;
+		else if (cles == 0)
+		{
+			cles = 1;
+			comp++;
+		}
+		s1++;
+	}
+	return (comp);
+}
+
+static char	**liberar(char const **arr, int j)
+{
+	while (j > 0)
+	{
+		--j;
+		free((void *)arr[j]);
+	}
+	free(arr);
+	return (NULL);
+}
+
+static char	**create_arr(char const *s, char **arr, char c, int len)
 {
 	int	i;
 	int	j;
+	int	k;
 
 	i = 0;
 	j = 0;
-	while (s[i] != '\0')
+	while (s[i] != '\0' && j < len)
 	{
-		if (s[i] == c)
-			++j;
-		++i;
+		k = 0;
+		while (s[i] == c)
+			++i;
+		arr[j] = (char *)malloc(sizeof(char) * str_count(s, c, i) + 1);
+		if (arr[j] == NULL)
+			return (liberar((char const **) arr, j));
+		while (s[i] != '\0' && s[i] != c)
+		{
+			arr[j][k] = s[i];
+			++k;
+			++i;
+		}
 	}
-	return (strlen(s) - j);
+	arr[j] = 0;
+	return (arr);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
-	int		len;
-	int		pos;
 	char	**arr;
+	int		len;
 
-	i = 0;
-	pos = 0;
-	arr = (char **)malloc(sizeof(char *) * str_len(s, c));
-	while (s[i] != '\0')
-	{
-		len = str_count(&s[i], c);
-		if (s[i] == c)
-		{
-			add_str(&s[i], arr, len, pos);
-			++pos;
-		}
-		++i;
-	}
-	return (arr);
+	if (s == NULL)
+		return (NULL);
+	len = rem_del(s, c);
+	arr = (char **)malloc(sizeof(char *) * (len + 1));
+	if (arr == NULL)
+		return (NULL);
+	return (create_arr(s, arr, c, len));
 }
 
 int	main(void)
