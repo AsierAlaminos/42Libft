a719315425f17f5245a3fc488733bec4531a1539
Asier Alaminos <aalamino@c2r16s2.42madrid.com>
2023-03-22 18:41:49 +0100

    update

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: aalamino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:17:40 by aalamino          #+#    #+#             */
-/*   Updated: 2023/03/22 18:23:18 by aalamino         ###   ########.fr       */
+/*   Updated: 2023/03/22 18:41:18 by aalamino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 t_list *ft_lstnew(void *content)
 {
+	t_list lista;
 
+	lista = (t_list*)malloc(sizeof(t_list));
+	if (!lista)
+		return (0);
+	lista->content=(void *)content;
+	return (lista);
 }
